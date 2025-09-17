7347fbe012942f383b62f333550e16bfaddc1e5a
Lou Springer <lou@louspringer.com>
2025-09-17 07:09:16 -0600

    PDCA: validate Directus data, scan duplicates, prep sync; ignore stray dup files

---
+++ src/rm_ddd/core/interface_registry 2.py
@@ -0,0 +1,59 @@
+from .interface_registry_part_1 import *
+from .interface_registry_part_2 import *
+from .interface_registry_part_3 import *
+from .interface_registry_part_4 import *
+from .interface_registry_part_5 import *
+from .interface_registry_part_6 import *
+from .interface_registry_part_7 import *
+from .interface_registry_part_8 import *
+from .interface_registry_part_9 import *
+from .interface_registry_part_10 import *
+from .interface_registry_part_11 import *
+from .interface_registry_part_12 import *
+from .interface_registry_part_13 import *
+from .interface_registry_part_14 import *
+from .interface_registry_part_15 import *
+from .interface_registry_part_16 import *
+from .interface_registry_part_17 import *
+from .interface_registry_part_18 import *
+from .interface_registry_part_19 import *
+from .interface_registry_part_20 import *
+from .interface_registry_part_21 import *
+from .interface_registry_part_22 import *
+from .interface_registry_part_23 import *
+from .interface_registry_part_24 import *
+from .interface_registry_part_25 import *
+from .interface_registry_part_26 import *
+from .interface_registry_part_27 import *
+from .interface_registry_part_28 import *
+from .interface_registry_part_29 import *
+from .interface_registry_part_30 import *
+from .interface_registry_part_31 import *
+from .interface_registry_part_32 import *
+from .interface_registry_part_33 import *
+from .interface_registry_part_34 import *
+from .interface_registry_part_35 import *
+from .interface_registry_part_36 import *
+from .interface_registry_part_37 import *
+from .interface_registry_part_38 import *
+from .interface_registry_part_39 import *
+from .interface_registry_part_40 import *
+from .interface_registry_part_41 import *
+from src.rm_ddd.core.health import ModuleHealth
+
+    def register_module(self, registry):
+        """Register module with registry."""
+        metadata = self.get_interface_metadata()
+        if hasattr(registry, 'register'):
+            registry.register(metadata)
+            
+    def get_interface_metadata(self):
+        """Get interface metadata for registry."""
+        return {
+            'module_id': getattr(self, 'module_id', self.__class__.__name__),
+            'interface_type': self.__class__.__name__,
+            'version': '1.0.0',
+            'dependencies': [],
+            'capabilities': []
+        }
+
